ba44a746fbb98832681ab1a19caf94007f9eb0bf
root <titas20031996@gmail.com>
2024-07-30 04:33:57 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rsyslog_client_assistant',
-    version='0.1.1',
+    version='0.1.4',
     description='Generate client-side configuration for rsyslog',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
